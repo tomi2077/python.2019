8df28379b4dc984bb926c857954c0d1516164035
Rails Udemy Course <tominiyiajayi@gmail.com>
2019-10-25 20:38:37 +0100

    Second

--- Date_22_10_19.py
+++ Date_22_10_19.py
@@ -1,6 +1,13 @@
-x = 0
-while x < 10:
-    if x == 5:
-        break
-    print (x)
-    x +=1
+def addition(a,b):
+    print(a * b)
+
+addition(2,2)
+
+def age_calculator(age):
+    if age < 50:
+        print "You are still young"
+    elif age > 50 and age < 69:
+        print "You re not getting younger anymore"
+    else:
+        print "You are old but can still achieve something"
+age_calculator(80)
